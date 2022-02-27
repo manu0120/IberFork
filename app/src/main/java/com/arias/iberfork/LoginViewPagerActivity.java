8b1af92ac650c6e84564d1c428023fbf75c6e517
manu0120 <manu0120sp@gmail.com>
2022-02-27 01:18:31 +0100

    Intentando implementar TabActivity con fragments

---
+++ app/src/main/java/com/arias/iberfork/LoginViewPagerActivity.java
@@ -0,0 +1,47 @@
+package com.arias.iberfork;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class LoginViewPagerActivity extends AppCompatActivity {
+    ViewPager2 viewPager2;
+    TabLayout tablayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_view_pager);
+
+        viewPager2=findViewById(R.id.viewPager);
+        tablayout=findViewById(R.id.tabLayout);
+
+        //viewPager2.setAdapter();
+    }
+
+
+
+    /*@NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position){
+            case 0:
+                return new FragmentLogin();
+            case 1:
+                return new FragmentSignUp();
+            //Se ejecuta si el resultado de la expresión no coincide con ningún case
+            default:
+                return new Fragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }*/
+}
