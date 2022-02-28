b6407175b676e0255cb45b823db8678253536180
manu0120 <manu0120sp@gmail.com>
2022-02-28 02:17:51 +0100

    new activities and new features to the recyclerView added

--- app/src/main/java/com/arias/iberfork/MiAdaptador.java
+++ app/src/main/java/com/arias/iberfork/MiAdaptador.java
@@ -1,12 +1,15 @@
 package com.arias.iberfork;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -16,6 +19,7 @@ public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.ViewHolder> {
     private LayoutInflater inflador;
     private List<String> dataTitulos;
     private List<String> dataDescripcion;
+    private CardView cardview;
 
     MiAdaptador(Context contexto, List<String> dataTitulos, List<String> dataDescripcion){
         this.inflador=LayoutInflater.from(contexto);
@@ -34,11 +38,47 @@ public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.ViewHolder> {
     //actualiza los datos de un ViewHolder ya existente
     @Override
     public void onBindViewHolder(@NonNull MiAdaptador.ViewHolder holder, int position) {
-        String titulo= dataTitulos.get(position);
+        String titulo = dataTitulos.get(position);
         holder.textTitulo.setText(titulo);
 
-        String descripcion= dataDescripcion.get(position);
+        String descripcion = dataDescripcion.get(position);
         holder.textDescripcion.setText(descripcion);
+
+        holder.cardView.findViewById(R.id.btnDetalles).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (holder.getAdapterPosition()){
+                    case 1:
+                        Intent intent=new Intent(view.getContext(), BarrilActivity.class);
+                        view.getContext().startActivity(intent);
+                        break;
+                    default:
+                        Intent intent2=new Intent(view.getContext(), BarrilActivity.class);
+                        view.getContext().startActivity(intent2);
+                        break;
+                }
+            }
+        });
+        /*holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button btnDetalles=view.findViewById(R.id.btnDetalles);
+
+
+                        switch (holder.getAdapterPosition()){
+                            case 1:
+                                Intent intent=new Intent(view.getContext(), BarrilActivity.class);
+                                view.getContext().startActivity(intent);
+                                break;
+                            default:
+                                Intent intent2=new Intent(view.getContext(), BarrilActivity.class);
+                                view.getContext().startActivity(intent2);
+                                break;
+                        }
+
+                }
+            });
+        */
     }
 
     //cuenta el número de registros para que el adaptador sepa cuando hemos llegado al final
@@ -50,11 +90,12 @@ public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.ViewHolder> {
         RecyclerView.ViewHolder */
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView textTitulo, textDescripcion;
+        CardView cardView;
         ViewHolder (View item){
             super(item);
             textTitulo=item.findViewById(R.id.tvTitulo);
             textDescripcion=item.findViewById(R.id.tvDetailsSub);
-
+            cardView=item.findViewById(R.id.cvRow);
         }
     }
 }
