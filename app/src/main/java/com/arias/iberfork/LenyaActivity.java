5dca56ebc73cd356a923d0179a5be2235ded00aa
manu0120 <manu0120sp@gmail.com>
2022-03-02 00:58:27 +0100

    all restaurants layout added

---
+++ app/src/main/java/com/arias/iberfork/LenyaActivity.java
@@ -0,0 +1,14 @@
+package com.arias.iberfork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LenyaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lenya);
+    }
+}
